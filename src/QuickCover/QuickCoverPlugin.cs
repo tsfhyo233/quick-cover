d944c25a902f13246aaec0c1b3414d1cc1de088d
tsfhyo233 <tsfhyo233@gmail.com>
2026-05-18 23:01:22 +0900

    修复图标bug

--- src/QuickCover/QuickCoverPlugin.cs
+++ src/QuickCover/QuickCoverPlugin.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Playnite.SDK;
 using Playnite.SDK.Models;
 using Playnite.SDK.Plugins;
@@ -20,6 +24,7 @@ namespace QuickCover
         private readonly SetBackgroundFromFileAction setBackgroundFromFileAction;
         private readonly ApplyDefaultImagesAction applyDefaultImagesAction;
         private readonly ApplyDefaultImagesAction applyDefaultUrlImagesAction;
+        private readonly string topPanelIconPath;
 
         public QuickCoverPlugin(IPlayniteAPI api) : base(api)
         {
@@ -37,6 +42,7 @@ namespace QuickCover
             setBackgroundFromFileAction = new SetBackgroundFromFileAction(api, imageImportService);
             applyDefaultImagesAction = new ApplyDefaultImagesAction(api, imageImportService, settings, DefaultImageSourceMode.PreferLocalThenUrl);
             applyDefaultUrlImagesAction = new ApplyDefaultImagesAction(api, imageImportService, settings, DefaultImageSourceMode.UrlOnly);
+            topPanelIconPath = Path.Combine(Path.GetDirectoryName(GetType().Assembly.Location), "source", "icon.png");
         }
 
         public override Guid Id => PluginId;
@@ -80,6 +86,59 @@ namespace QuickCover
                 MenuSection = "Quick Cover",
                 Action = menuArgs => applyDefaultUrlImagesAction.Execute(menuArgs.Games)
             };
+        }
+
+        public override IEnumerable<TopPanelItem> GetTopPanelItems()
+        {
+            yield return new TopPanelItem
+            {
+                Title = "Quick Cover",
+                Icon = CreateTopPanelIcon(),
+                Activated = () => applyDefaultImagesAction.Execute(GetSelectedGames()),
+                Visible = true
+            };
+        }
+
+        private IEnumerable<Game> GetSelectedGames()
+        {
+            return PlayniteApi.MainView.SelectedGames?.ToList() ?? new List<Game>();
+        }
+
+        private object CreateTopPanelIcon()
+        {
+            if (!File.Exists(topPanelIconPath))
+            {
+                return null;
+            }
+
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.UriSource = new Uri(topPanelIconPath, UriKind.Absolute);
+            bitmap.EndInit();
+            bitmap.Freeze();
+
+            var iconSource = new CroppedBitmap(bitmap, CreateCenteredSquareCrop(bitmap.PixelWidth, bitmap.PixelHeight, 0.68));
+            iconSource.Freeze();
+
+            return new Image
+            {
+                Source = iconSource,
+                Width = 22,
+                Height = 22,
+                Stretch = Stretch.Uniform,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center,
+                RenderTransform = new TranslateTransform(2, 2)
+            };
+        }
+
+        private static System.Windows.Int32Rect CreateCenteredSquareCrop(int pixelWidth, int pixelHeight, double cropRatio)
+        {
+            var squareSize = (int)Math.Round(Math.Min(pixelWidth, pixelHeight) * cropRatio);
+            var x = Math.Max(0, (pixelWidth - squareSize) / 2);
+            var y = Math.Max(0, (pixelHeight - squareSize) / 2);
+            return new System.Windows.Int32Rect(x, y, squareSize, squareSize);
         }
     }
 }
